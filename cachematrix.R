f805e87866d0ea69406328d41af7b6cfd8b1c56d
DRLR <gdb17maker@gmail.com>
2014-04-23 10:10:51 +0200

    Update cachematrix.R
    Here is the updated version for the Peer Assessments: Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This are two functions:
+## function 1 makes a matrix, while function 2 will inverse the matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function takes an argument x of type matrix
+makeCacheMatrix <- function(x = matrix()) { 
+        ##Initiate matrix varible
+        m <- NULL
+        ##Set the function.In other words...the function exists
+        set <- function(y) {
+                        x <<- y
+                        m <<- NULL
+                }
+        ##use the function
+        get <- function() x
+        #
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        ##reorder all rows using rev. FOR MORE INFO SEE ?REV
+        list <- apply(data, 2, rev)
+        ##make the matrix m from the list
+        m <- as.matrix(list)
+        #return results
+        m
 }
 
 
-## Write a short comment describing this function
-
+## This function takes an matrix and makes the invers matrix using rev()
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## query the x vector's cache 
+        m <- x$getmean()
+        ##If the matrix m contains something statement is fullfilled
+        if(!is.null(m)) {
+                ##Therefore, we return a message and the matrix
+                message("getting cached data")
+                return(m)
+        }
+        ##get the data for invers ordering
+        data <- x$get()
+        ##make the matrix m from the list
+        m <- as.matrix(data)
+        #return results
+        m
 }
